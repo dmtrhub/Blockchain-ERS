98a930532011ae31bf2a93b7f75b14b658d58d00
vukovicd <vukovicc.d@gmail.com>
2024-05-21 19:43:57 +0200

    Blockchain

---
+++ Blockchain/Data/Blockchain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockchain.Data
+{
+    public class Blockchain
+    {
+        public List<Block> Chain { get; }
+
+        private Blockchain()
+        {
+            Chain = new List<Block>();
+        }
+
+        public void AddBlock(Block newBlock)
+        {
+            Chain.Add(newBlock);
+        }
+    }
+}
