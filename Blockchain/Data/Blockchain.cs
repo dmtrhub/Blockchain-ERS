342cb5b9e5adff03eb00d59790b240ca0cbca2f3
vukovicd <vukovicc.d@gmail.com>
2024-05-22 20:53:33 +0200

    Upgrade blockchain - singleton pattern

--- Blockchain/Data/Blockchain.cs
+++ Blockchain/Data/Blockchain.cs
@@ -8,11 +8,23 @@ namespace Blockchain.Data
 {
     public class Blockchain
     {
+        private static Blockchain instance;
+        public static Blockchain Instance => instance ??= new Blockchain();
         public List<Block> Chain { get; }
 
         private Blockchain()
         {
-            Chain = new List<Block>();
+            Chain = new List<Block> { CreateGenesisBlock() };
+        }
+
+        private Block CreateGenesisBlock()
+        {
+            return new Block(0, "Genesis Block", "0");
+        }
+
+        public Block GetLatestBlock()
+        {
+            return Chain[^1];
         }
 
         public void AddBlock(Block newBlock)
