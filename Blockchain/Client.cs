5e46190886bf032bc4359f4bbd8c3f95ecf0fef5
vukovicd <vukovicc.d@gmail.com>
2024-05-22 20:54:37 +0200

    Client, "Sending data" to SmartContract

--- Blockchain/Client.cs
+++ Blockchain/Client.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace Blockchain
 {
     public class Client
-    {      
+    {
+        private SmartContract smartContract;
+        public string Id { get; }
+
+        public Client(string id, SmartContract smartContract)
+        {
+            Id = id;
+            this.smartContract = smartContract;
+        }
+
+        public void SendData(string data)
+        {
+            Console.WriteLine($"\nClient {Id} sending data...\n");
+        }
     }
 }
