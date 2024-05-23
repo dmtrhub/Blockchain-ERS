a9b781681c56cd230b830e5d595570290660bd93
vukovicd <vukovicc.d@gmail.com>
2024-05-23 19:47:53 +0200

    Blockchain changes, thread safe singleton*

--- Blockchain/Data/Blockchain.cs
+++ Blockchain/Data/Blockchain.cs
@@ -8,27 +8,45 @@ namespace Blockchain.Data
 {
     public class Blockchain
     {
-        private static Blockchain instance;
-        public static Blockchain Instance => instance ??= new Blockchain();
-        public List<Block> Chain { get; }
+        private Blockchain instance = null;
+        private readonly object lockObject = new object();
+
+        public IList<Block> Chain { get; }
+        public int Digits { get; set; } = 3;
 
         private Blockchain()
         {
             Chain = new List<Block> { CreateGenesisBlock() };
         }
 
+        public Blockchain Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Blockchain();
+                    }
+                    return instance;
+                }
+            }
+        }
+
         private Block CreateGenesisBlock()
         {
-            return new Block(0, "Genesis Block", "0");
+            return new Block(0, DateTime.Now, "Genesis Block", "0");
         }
 
         public Block GetLatestBlock()
         {
-            return Chain[^1];
+            return Chain[Chain.Count - 1];
         }
 
         public void AddBlock(Block newBlock)
         {
+            newBlock.PreviousHash = GetLatestBlock().Hash;
             Chain.Add(newBlock);
         }
     }
